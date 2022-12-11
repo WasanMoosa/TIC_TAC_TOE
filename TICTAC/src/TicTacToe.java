a54bd6a95324b6ffc6b46637e5ef5d7a60e77e21
WasanMoosa <wasssoon@gmail.com>
2022-12-12 01:51:13 +0400

    Phase 1 project done

--- TICTAC/src/TicTacToe.java
+++ TICTAC/src/TicTacToe.java
@@ -1,56 +1,143 @@
 import javax.lang.model.element.Element;
+
 import java.util.Scanner;
 
 public class TicTacToe {
 
 
 
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		//[] movepos = new Integer[9];
 		Scanner sc= new Scanner(System.in);
-		int i=0;
-		int mov1=0;
-		int mov2=0;
-		Object[] movepos= {1,2,3,4,5,6,7,8,9};
+
+		char mov1='0';
+		char mov2='0';
+		char[] movepos= {'1','2','3','4','5','6','7','8','9'};
+		boolean check =false;  //Check if the position is already taken 
+
+		System.out.println("Start playing TIC TAC TOE");	
 
 
-		
-		
-		
-		for (int r=0; r<9; r++) {
-			if (mov1==(int) movepos[r]) {
-				movepos[r]="X";}
-				//Displaying the moves box
-				System.out.println("Start playing TIC TAC TOE");	
-				for (; i<3; i++) {
-					System.out.print(" "+movepos[i]+" |");		
-				}
-				System.out.print("\n");
-				System.out.print("-------------");
-				System.out.print("\n");
-				for (; i<6; i++) {
-					System.out.print(" "+movepos[i]+" |");	
-				}
-				System.out.print("\n");
-				System.out.print("-------------");
-				System.out.print("\n");
-				for (; i<9; i++) {
-					System.out.print(" "+movepos[i]+" |");	
-				}
+		// Loop for continuity of the game 
+		for (int count=0; count<10; count++) {
 
-				System.out.println("\n");
-				System.out.println("Player 1 Symbol: X");
-				System.out.println("Player 2 Symbol: O");
-				System.out.print("Player 1, Make a move, Enter position: ");
-			    mov1=sc.nextInt();
-
-
+			//Print out box
+			int i=0;   
+			for (; i<3; i++) {
+				System.out.print(" "+movepos[i]+" |");		
 			}
+			System.out.print("\n");
+			System.out.print("-------------");
+			System.out.print("\n");
+			for (; i<6; i++) {
+				System.out.print(" "+movepos[i]+" |");	
+			}
+			System.out.print("\n");
+			System.out.print("-------------");
+			System.out.print("\n");
+			for (; i<9; i++) {
+				System.out.print(" "+movepos[i]+" |");	
+			}
+			// Win conditions
+			if (movepos[0]== movepos[1] && movepos[0]== movepos[2] || movepos[0]== movepos[3] && movepos[3]== movepos[6] ||
+					movepos[1]== movepos[4] && movepos[1]== movepos[7] || movepos[2]== movepos[5] && movepos[5]== movepos[8] ||
+					movepos[3]== movepos[4] && movepos[4]== movepos[5] || movepos[6]== movepos[7] && movepos[7]== movepos[8] ||
+					movepos[2]== movepos[4] && movepos[4]== movepos[6] || movepos[0]== movepos[4] && movepos[4]== movepos[8] ){
 
-		}
+				System.out.print("\n");
+				System.out.print("Well done");
+				break;}
+			System.out.println("\n");
+			System.out.println("Player 1 Symbol: X");
+			System.out.println("Player 2 Symbol: O");
+
+
+			check =false;
+
+			if (count !=9) {
+				// For player 1 taking input, checking and replacing symbol
+				if (count%2 == 0 ) {
+					System.out.print("Player 1, Make a move, Enter position: ");
+					mov1=sc.next().charAt(0); 
+					while (check==false){
+						for (int d=0; d<9; d++) {
+							if (mov1==movepos[d]) {
+								check=true; 
+								break;
+							}}
+						if (check==false) {
+							System.out.print("Player 1, Invalid input, re-enter position: ");
+							mov1=sc.next().charAt(0); }	
+					}
+
+				}
+
+				// For player 2 taking input, checking and replacing symbol
+				else {
+					System.out.print("Player2, Make a move Enter position ");
+
+					mov2=sc.next().charAt(0); 
+
+					while (check==false){
+						for (int d=0; d<9; d++) {
+							if (mov2==movepos[d]) {
+								check=true; 
+								break;
+							}}
+						if (check==false) {
+							System.out.print("Player 2, Invalid input, re-enter position:");
+							mov2=sc.next().charAt(0); }	
+
+					}}
+
+				// Change the integer of array into symbol 
+				for (int r=0; r<9; r++) {
+
+					// 
+					if  (count%2== 0 ) {
+						if (mov1== movepos[r]) {
+							movepos[r]='X';}
+					}
+
+
+					if  (count%2!= 0 ){
+
+						if (mov2== movepos[r]) {
+							movepos[r]='O';}
+
+					}}
+
+
+			}}
+		System.out.print("The End");
 
 
 	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
