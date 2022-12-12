3a1c4c2d59f16ac101c02e77b9ae8cf2930d454b
Flotss <manginf54@gmail.com>
2022-12-12 14:24:40 +0100

    https://github.com/WasanMoosa/TIC_TAC_TOE/issues/11
    https://github.com/WasanMoosa/TIC_TAC_TOE/issues/2

--- TICTAC/src/TicTacToe.java
+++ TICTAC/src/TicTacToe.java
@@ -1,76 +1,67 @@
-import javax.lang.model.element.Element;
-
 import java.util.Scanner;
 
 public class TicTacToe {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		Character[] symbol = new Character[2];
-		char mov1='0';
-		char mov2='0';
-		boolean draw=false;
-		char[] movepos= {'1','2','3','4','5','6','7','8','9'};
-		boolean check =false;  //Check if the position is already taken 
+		char mov1 = '0';
+		char mov2 = '0';
+		boolean draw = false;
+		char[] movepos = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-		System.out.println("Start playing TIC TAC TOE");	
-		
-		System.out.println("Player 1, choose your symbol ");	
-		symbol[0]= sc.next().charAt(0);
-		System.out.println("Player 2, choose your symbol ");
-		symbol[1]= sc.next().charAt(0);
+		System.out.println("Start playing TIC TAC TOE");
+
+		System.out.println("Player 1, choose your symbol ");
+		symbol[0] = sc.next().charAt(0);
+
+		do {
+			System.out.println("Player 2, choose your symbol ");
+			symbol[1] = sc.next().charAt(0);
+			System.out.println("Symbol already taken, choose another one");
+		} while (symbol[1] == symbol[0]);
 
 		// Loop for continuity of the game 
-		for (int count=0; count<10; count++) {
+		for (int count = 0; count < 10; count++) {
 
 			//Print out box
-			int i=0;   
-			for (; i<3; i++) {
-				System.out.print(" "+movepos[i]+" |");		
+			int i = 0;
+			for (; i < 3; i++) {
+				System.out.print(" " + movepos[i] + " |");
 			}
 			System.out.print("\n");
 			System.out.print("-------------");
 			System.out.print("\n");
-			for (; i<6; i++) {
-				System.out.print(" "+movepos[i]+" |");	
+			for (; i < 6; i++) {
+				System.out.print(" " + movepos[i] + " |");
 			}
 			System.out.print("\n");
 			System.out.print("-------------");
 			System.out.print("\n");
-			for (; i<9; i++) {
-				System.out.print(" "+movepos[i]+" |");	
+			for (; i < 9; i++) {
+				System.out.print(" " + movepos[i] + " |");
 			}
 			// Win conditions
-			if (movepos[0]== movepos[1] && movepos[0]== movepos[2] || movepos[0]== movepos[3] && movepos[3]== movepos[6] ||
-					movepos[1]== movepos[4] && movepos[1]== movepos[7] || movepos[2]== movepos[5] && movepos[5]== movepos[8] ||
-					movepos[3]== movepos[4] && movepos[4]== movepos[5] || movepos[6]== movepos[7] && movepos[7]== movepos[8] ||
-					movepos[2]== movepos[4] && movepos[4]== movepos[6] || movepos[0]== movepos[4] && movepos[4]== movepos[8] ){
+			if (movepos[0] == movepos[1] && movepos[0] == movepos[2] || movepos[0] == movepos[3] && movepos[3] == movepos[6] ||
+					movepos[1] == movepos[4] && movepos[1] == movepos[7] || movepos[2] == movepos[5] && movepos[5] == movepos[8] ||
+					movepos[3] == movepos[4] && movepos[4] == movepos[5] || movepos[6] == movepos[7] && movepos[7] == movepos[8] ||
+					movepos[2] == movepos[4] && movepos[4] == movepos[6] || movepos[0] == movepos[4] && movepos[4] == movepos[8]) {
 
 				System.out.print("\n");
 				System.out.println("Well done");
-				draw =true;}
+				draw = true;
+			}
 			System.out.println("\n");
 			System.out.println("Player 1 Symbol: " + symbol[0]);
-			System.out.println("Player 2 Symbol: " +symbol[1]);
+			System.out.println("Player 2 Symbol: " + symbol[1]);
 
 
-			check =false;
-
-			if (count !=9) {
+			if (count != 9) {
 				// For player 1 taking input, checking and replacing symbol
-				if (count%2 == 0 ) {
+				if (count % 2 == 0) {
 					System.out.print("Player 1, Make a move, Enter position: ");
-					mov1=sc.next().charAt(0); 
-					while (check==false){
-						for (int d=0; d<9; d++) {
-							if (mov1==movepos[d]) {
-								check=true; 
-								break;
-							}}
-						if (check==false) {
-							System.out.print("Player 1, Invalid input, re-enter position: ");
-							mov1=sc.next().charAt(0); }	
-					}
+					mov1 = sc.next().charAt(0);
+					mov1 = checkInput(mov1, movepos, 1);
 
 				}
 
@@ -78,68 +69,68 @@ public class TicTacToe {
 				else {
 					System.out.print("Player2, Make a move Enter position ");
 
-					mov2=sc.next().charAt(0); 
+					mov2 = sc.next().charAt(0);
 
-					while (check==false){
-						for (int d=0; d<9; d++) {
-							if (mov2==movepos[d]) {
-								check=true; 
-								break;
-							}}
-						if (check==false) {
-							System.out.print("Player 2, Invalid input, re-enter position:");
-							mov2=sc.next().charAt(0); }	
+					mov2 = checkInput(mov2, movepos, 2);
 
-					}}
+					// Change the integer of array into symbol
+					for (int r = 0; r < 9; r++) {
 
-				// Change the integer of array into symbol 
-				for (int r=0; r<9; r++) {
+						if (count % 2 == 0) {
+							if (mov1 == movepos[r]) {
+								movepos[r] = symbol[0];
+							}
+						}
 
-					// 
-					if  (count%2== 0 ) {
-						if (mov1== movepos[r]) {
-							movepos[r]=	symbol[0];}
+
+						if (count % 2 != 0) {
+
+							if (mov2 == movepos[r]) {
+								movepos[r] = symbol[1];
+							}
+
+						}
 					}
+				}
 
-
-					if  (count%2!= 0 ){
-
-						if (mov2== movepos[r]) {
-							movepos[r]=	symbol[1];}
-
-					}}
-
+				if (count == 9 && draw == false) {
+					System.out.println("It is draw");
+				}
 
 			}
-			
-		if (count==9 && draw== false) {
-			System.out.println("It is draw");
+			System.out.print("The End");
+
+
 		}
-			
-		}
-		System.out.print("The End");
 
 
 	}
 
+	/**
+	 * Check if the input is valid
+	 * if valid, return the input
+	 * if not valid, ask for input again
+	 * @param mov the input
+	 * @param movepos the array of the position
+	 * @param numberPlayer the name of player
+	 * @return char the caracter of the input
+	 */
+	private static char checkInput(char mov, char[] movepos, int numberPlayer) {
+		Scanner sc = new Scanner(System.in);
+		boolean check = false;
+		while (!check) {
+			for (int d = 0; d < 9; d++) {
+				if (mov == movepos[d]) {
+					check = true;
+					break;
+				}
+			}
+			if (!check) {
+				System.out.print("Player " + numberPlayer + ", Invalid input, re-enter position:");
+				mov = sc.next().charAt(0);
+			}
+
+		}
+		return mov;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
